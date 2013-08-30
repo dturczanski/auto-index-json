8b8f210854f1d378027203daf2e8f754564452ff
Daniel Turczanski <turczanski@gmail.com>
2013-08-30 23:06:48 +0100

    Script file

---
+++ auto-index-json.js
@@ -0,0 +1,32 @@
+var express = require('express')
+  , fs = require('fs')
+  , app = express.createServer();
+
+var root = __dirname + '/public';
+
+express.directory.json = function(req, res, files, next, dir, showUp, icons){
+  var richFiles = files.map(function(file) {
+        var fileStats = fs.statSync(root + dir + '/' + file);
+        return {'name': file, 'isDirectory': fileStats.isDirectory()};
+  });
+  files = JSON.stringify({
+        'files': richFiles,
+        'dir': dir,
+        'next': next,
+        'showUp': showUp,
+        'icons': icons
+  });
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Length', files.length);
+
+  res.end(files);
+};
+
+app.configure(function() {
+  var hourMs = 1000*60*60;
+  app.use(express.directory(root));
+  app.use(express.static(root, { maxAge: hourMs }));
+  app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
+});
+
+app.listen(8080);
